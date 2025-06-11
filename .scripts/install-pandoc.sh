a7a5aa2425894d294a581d7ebf547e5e361f8db1
Yttehs <shetty.yttehs@outlook.com>
2025-06-11 12:09:23 +0800

    fix: download pandoc binary

---
+++ .scripts/install-pandoc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir tmp
+cd tmp || exit
+
+wget https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-amd64.tar.gz
+tar -xzf pandoc-3.7.0.2-linux-amd64.tar.gz
+
+mkdir bin
+
+cp pandoc-3.7.0.2/bin/* bin/.
