e0d003110d520dc09789a558fd6ef37fe0ec129c
Yttehs <shetty.yttehs@outlook.com>
2025-06-11 12:16:30 +0800

    fix: change wget to curl

--- .scripts/install-pandoc.sh
+++ .scripts/install-pandoc.sh
@@ -3,7 +3,7 @@
 mkdir tmp
 cd tmp || exit
 
-wget https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-amd64.tar.gz
+curl -L https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-amd64.tar.gz
 tar -xzf pandoc-3.7.0.2-linux-amd64.tar.gz
 
 mkdir bin
