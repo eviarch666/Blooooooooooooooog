e2ef0ad971226c7f2ce3da97e5f8c6cc99982be9
Yttehs <shetty.yttehs@outlook.com>
2025-06-11 12:26:16 +0800

    fix: curl -o

--- .scripts/install-pandoc.sh
+++ .scripts/install-pandoc.sh
@@ -3,8 +3,8 @@
 mkdir tmp
 cd tmp || exit
 
-curl -L https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-amd64.tar.gz
-tar -xzf pandoc-3.7.0.2-linux-amd64.tar.gz
+curl -L https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-amd64.tar.gz -o pandoc.tar.gz
+tar -xzf pandoc.tar.gz
 
 mkdir bin
 
